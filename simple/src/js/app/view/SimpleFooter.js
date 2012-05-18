a9dc118b98dff2dd79a516731921d044b551f03a
andy-richardson <andy.richardson@thomsonreuters.com>
2012-05-18 13:23:58 +0100

    Moved files from rishson for sample application

---
+++ simple/src/js/app/view/SimpleFooter.js
@@ -0,0 +1,56 @@
+define([
+	"rishson/widget/_Widget", //mixin
+	"rishson/Base", //mixin
+	"rishson/util/ObjectValidator", //validate
+	"dijit/_TemplatedMixin", //mixin
+	"dijit/_WidgetsInTemplateMixin", //mixin
+	"dojo/text!rishson/view/simpleFooter/SimpleFooter.html", //template
+	"dojo/_base/declare"    // declare + safeMixin
+], function (_Widget, Base, ObjectValidator, _TemplatedMixin, _WidgetsInTemplateMixin, template, declare) {
+
+	/**
+	 * @class
+	 * @name rishson.view.SimpleFooter
+	 * @description An example widget that acts as a footer to a single page application.
+	 */
+	return declare('app.view.SimpleFooter', [_Widget, Base, _TemplatedMixin, _WidgetsInTemplateMixin], {
+
+		templateString: template,
+
+		/**
+		 * @field
+		 * @name rishson.view.SimpleFooter.footerText
+		 * @type {string}
+		 * @description the text to place into the footer
+		 */
+		footerText: '',
+
+		/**
+		 * @field
+		 * @name rishson.view.SimpleFooter.footerLink
+		 * @type {string}
+		 * @description the href for the footer link
+		 */
+		footerLink: '',
+
+		/**
+		 * @constructor
+		 * @param {{footerText : string, footerLink : string}} params contains the footer link and footer text
+		 */
+		constructor: function (params) {
+			var criteria = [
+					{paramName: 'footerText', paramType: 'string'},
+					{paramName: 'footerLink', paramType: 'string'}
+				],
+				validator = new ObjectValidator(criteria);
+
+			if (validator.validate(params)) {
+				declare.safeMixin(this, params);
+			}
+			else {
+				validator.logErrorToConsole(params, 'Invalid params passed to the SimpleFooter.');
+				throw ('Invalid params passed to the SimpleFooter.');
+			}
+		}
+	});
+});
